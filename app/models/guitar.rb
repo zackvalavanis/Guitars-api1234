52990c98ecf5e3d9e7b25f6616f48d30b02c5e6d
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-28 13:12:27 -0500

    added functionality for multiple images

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,4 +1,15 @@
 class Guitar < ApplicationRecord
   has_many :images
   belongs_to :users
+
+
+  def images_with_default 
+    if images.length > 0 
+      return images
+    else 
+      return [
+        {id: 100, url: "https://cdn1.iconfinder.com/data/icons/media-player-icons-1/48/icon_random-512.png", guitar_id: 500, created_at: Time.now, updated_at: Time.now}
+      ]
+    end 
+  end 
 end
